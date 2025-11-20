785403b7155fcfd23ea6f5099902938f6c32668f
Chodavarapu Naga Viswanadha Avinash <aavinash@newrelic.com>
2025-11-20 16:20:22 +0530

    refactor: update ARM64 region to us-west-1 and uncomment build steps for arm64

--- scripts/testlayer.sh
+++ scripts/testlayer.sh
@@ -15,7 +15,7 @@ LAYER_NAME_PREFIX=${LAYER_NAME_PREFIX:-"NRTestRustExtension"}
 
 BUCKET_PREFIX=${BUCKET_PREFIX:-"nr-extension-test-layers"}
 REGIONS_X86_64=${REGIONS_X86_64:-"us-west-1"}
-REGIONS_ARM64=${REGIONS_ARM64:-"us-west-2"}
+REGIONS_ARM64=${REGIONS_ARM64:-"us-west-1"}
 
 BIN_NAME="newrelic-lambda-extension"
 DIST_DIR="$ROOT_DIR/dist"
@@ -244,27 +244,27 @@ main() {
     publish_layer "$DIST_DIR/${BIN_NAME}-x86_64.zip" "$region" "extension" "x86_64" "${LAYER_NAME_PREFIX}X86"
   done
 
-  # Package and publish single Python layer
-  build_python_layer_all "$target_x86"
-  for region in $REGIONS_X86_64; do
-    publish_layer "$DIST_DIR/python-all-x86_64.zip" "$region" "python" "x86_64" "${LAYER_NAME_PREFIX}PythonX86"
-  done
+  # # Package and publish single Python layer
+  # build_python_layer_all "$target_x86"
+  # for region in $REGIONS_X86_64; do
+  #   publish_layer "$DIST_DIR/python-all-x86_64.zip" "$region" "python" "x86_64" "${LAYER_NAME_PREFIX}PythonX86"
+  # done
 
-  # Package and publish single Node.js layer
-  build_nodejs_layer_all "$target_x86"
-  for region in $REGIONS_X86_64; do
-    publish_layer "$DIST_DIR/nodejs-all-x86_64.zip" "$region" "nodejs" "x86_64" "${LAYER_NAME_PREFIX}NodejsX86"
-  done
+  # # Package and publish single Node.js layer
+  # build_nodejs_layer_all "$target_x86"
+  # for region in $REGIONS_X86_64; do
+  #   publish_layer "$DIST_DIR/nodejs-all-x86_64.zip" "$region" "nodejs" "x86_64" "${LAYER_NAME_PREFIX}NodejsX86"
+  # done
 
   #--- Build for arm64 ---
-  # local target_arm="aarch64-unknown-linux-musl"
-  # build_extension "$target_arm"
+  local target_arm="aarch64-unknown-linux-musl"
+  build_extension "$target_arm"
 
-  # # Package and publish standalone extension
-  # package_extension_layer "$target_arm"
-  # for region in $REGIONS_ARM64; do
-  #   publish_layer "$DIST_DIR/${BIN_NAME}-aarch64.zip" "$region" "extension" "arm64" "${LAYER_NAME_PREFIX}ARM64"
-  # done
+  # Package and publish standalone extension
+  package_extension_layer "$target_arm"
+  for region in $REGIONS_ARM64; do
+    publish_layer "$DIST_DIR/${BIN_NAME}-aarch64.zip" "$region" "extension" "arm64" "${LAYER_NAME_PREFIX}ARM64"
+  done
 
   # # Package and publish single Python layer
   # build_python_layer_all "$target_arm"
